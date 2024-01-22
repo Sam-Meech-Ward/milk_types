837b15ab5f29192ab7fa591e38abd9301588cb26
sam <sam@meech-ward.me>
2024-01-22 12:56:57 -0800

    Add form

--- frontend/src/routes/create.component.tsx
+++ frontend/src/routes/create.component.tsx
@@ -4,13 +4,35 @@ import { useNavigate } from "@tanstack/react-router";
 
 import { useMutation } from "@tanstack/react-query";
 
+import { useForm } from "@tanstack/react-form";
+
+import { zodValidator } from "@tanstack/zod-form-adapter";
+import { z } from "zod";
+
+const MilkType = z.object({
+  type: z.string().min(3).max(100),
+  rating: z.number().min(0).max(5),
+});
+type MilkType = z.infer<typeof MilkType>;
+
 export const component = function CreatePage() {
-  const [type, setType] = useState("");
-  const [rating, setRating] = useState(0);
+  const form = useForm({
+    defaultValues: {
+      type: "",
+      rating: 0,
+    },
+    onSubmit: async ({ value }) => {
+      // wait
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      await mutateAsync(value);
+      navigate({ to: "/" });
+    },
+    validatorAdapter: zodValidator,
+  });
 
   const { error, isPending, mutateAsync } = useMutation({
     mutationKey: ["create-milk"],
-    mutationFn: async () => {
+    mutationFn: async ({ type, rating }: MilkType) => {
       const response = await fetch("/api/milks", {
         method: "POST",
         headers: {
@@ -24,43 +46,86 @@ export const component = function CreatePage() {
 
   const navigate = useNavigate({ from: "create" });
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    await mutateAsync();
-
-    navigate({ to: "/" });
-  };
-
   return (
     <>
       {error && <p className="text-red-500">Error: try again later</p>}
-      <form className="mt-10 flex flex-col" onSubmit={handleSubmit}>
-        <label className="block mb-2">Type</label>
-        <input
-          className="border border-gray-400 rounded px-2 py-1 mb-4"
-          type="text"
-          placeholder="Type"
-          value={type}
-          onChange={(event) => setType(event.target.value)}
-        />
 
-        <label className="block mb-2">Rating</label>
-        <input
-          className="border border-gray-400 rounded px-2 py-1 mb-4"
-          type="number"
-          placeholder="Rating"
-          value={rating}
-          onChange={(event) => setRating(Number(event.target.value))}
-        />
-
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-          disabled={isPending}
+      <form.Provider>
+        <form
+          className="mt-10 flex flex-col"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            void form.handleSubmit();
+          }}
         >
-          {isPending ? "..." : "Create"}
-        </button>
-      </form>
+          <form.Field
+            name="type"
+            validators={{
+              onChange: MilkType.shape.type,
+            }}
+            children={(field) => (
+              <>
+                <label className="block mb-2">Type</label>
+                <input
+                  className="border border-gray-400 rounded px-2 py-1 mb-4"
+                  type="text"
+                  placeholder="Type"
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.errors && (
+                  <em role="alert">{field.state.meta.errors.join(", ")}</em>
+                )}
+              </>
+            )}
+          />
+
+          <form.Field
+            name="rating"
+            validators={{
+              onChange: MilkType.shape.rating,
+            }}
+            children={(field) => (
+              <>
+                <label className="block mb-2">Rating</label>
+                <input
+                  className="border border-gray-400 rounded px-2 py-1 mb-4"
+                  type="number"
+                  placeholder="Rating"
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                {field.state.meta.errors && (
+                  <em role="alert">{field.state.meta.errors.join(", ")}</em>
+                )}
+              </>
+            )}
+          />
+
+          {/* <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            disabled={isPending}
+          >
+            {isPending ? "..." : "Create"}
+          </button> */}
+
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+                type="submit"
+                disabled={!canSubmit || isSubmitting}
+              >
+                {isSubmitting ? "..." : "Submit"}
+              </button>
+            )}
+          ></form.Subscribe>
+        </form>
+      </form.Provider>
     </>
   );
 };
